b45fd775f543efde60c2e9c5ade5f04caa555c07
SergioStasevich <kangurek1991@gmail.com>
2022-11-09 19:07:25 +0100

    Initial Commit

---
+++ instApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instApp
+//
+//  Created by Siarhei Stasevich on 09/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
